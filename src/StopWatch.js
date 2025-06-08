1de1b0ae5862a741aeb1aa494d38f13399f5d03a
Ritesh Dwivedi <ritesh@techbrj.com>
2025-06-08 14:06:00 +0530

    Implementation done

---
+++ src/StopWatch.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function StopWatch() {
+  const [timeInSeconds, setTimeInSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTimeInSeconds((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalRef.current);
+    }
+
+    // Cleanup interval on unmount
+    return () => clearInterval(intervalRef.current);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTimeInSeconds(0);
+  };
+
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <div>
+        Time : {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </div>
+      {isRunning ? (
+        <button onClick={handleStop}>Stop</button>
+      ) : (
+        <button onClick={handleStart}>Start</button>
+      )}
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
+
+export default StopWatch;
